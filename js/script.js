701460fd9d0f84f258a76b14d117baf2a5120a74
Devair Martins <devair.martins@gmail.com>
2022-10-19 14:37:37 -0300

    ta dificil

--- js/script.js
+++ js/script.js
@@ -32,26 +32,51 @@ function textoDecodif(){
     botao.textContent = "Decodificar";
 }
 
-// function imprimir(){
+function imprimir(){
 
-//     let fraseInicial = document.getElementById("msg_in");
-//     let alfa = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-//     let incremento = getElementById("increment");
-//     let x = parseInt(incremento);
-//     let fraseFinal = [];
+    let alfa = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    let fraseInicial = document.getElementById("msg_in");
+//    alert(fraseInicial.value); teste da variavel
+    let incremento = document.getElementById("increment");
+//    console.log("incremento.value = " +incremento.value); //teste da variavel
+    let xyx = parseInt(incremento.value);
+//    console.log("xyx já convertido = "+xyx);
+//    console.log(typeof xyx);
+    //teste da variavel
+    let fraseFinal = [];
+
+    for (let i=0; i<fraseInicial.length; i++){
+        for(let j=0; j<alfa.length; j++){
+            if(fraseInicial[i] == alfa[j]){
+                fraseFinal[i] = alfa[j];
+                console.log(i);
+                console.log(j);
+            }
+        }
+    }
+
+
+    // for(let i=0; i<5; i++){
+    //     fraseFinal[i] = alfa[(i+xyx)];
+    // }
+
+    // alert(fraseFinal.join(""));
+    // alert(fraseInicial.value);
 
 //     for (let i = 0; i < fraseInicial.length; i++){
-//         if(fraseInicial[i] != ' '){
+// //        if(fraseInicial[i] != ' '){
 //             for (let j = 0; j < alfa.length; j++){
 //                 if (fraseInicial[i] == alfa[j]){
-//                     fraseFinal[i] = alfa[(j + x) % alfa.length];
+//                     alert(j);
+//                     alert(xyx);
+//                     fraseFinal[i] = alfa[(j + xyx) % alfa.length];
 //                     break;
 //                 }
 //             }
-//         }else{
-//             fraseFinal[i] = ' ';
-//         }
+// //        }else{
+// //            fraseFinal[i] = ' ';
+// //        }
 //     }
-//     alert(fraseFinal);
-    
-// }
+//     alert(fraseFinal.join(""));
+
+}
